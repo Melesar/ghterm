97c119d1ae8e2e4161cb7c4fc1ad0a167625c4a9
Sergey Pikhota <sergey.pikhota@protonmail.com>
2021-05-10 12:34:11 +0200

    Draw the main screen tab

--- src/frontend/main_screen.rs
+++ src/frontend/main_screen.rs
@@ -2,6 +2,8 @@ use crate::app::events::AppEvent;
 use crate::backend::pr;
 use super::screen::*;
 
+use termion::cursor::Goto;
+
 use std::sync::mpsc;
 use std::collections::HashMap;
 use std::io::Write;
@@ -19,30 +21,40 @@ pub struct MainScreen  {
 impl MainScreen {
     pub fn new (event_sender: mpsc::Sender<AppEvent>) -> Self {
         let mut tabs : HashMap<String, Box<dyn ApplicationScreen>> = HashMap::new();
-        tabs.insert(String::from("1. Conversation"), Box::new(ConversationTab::new()));
+        tabs.insert(String::from("Conversation"), Box::new(ConversationTab::new()));
         MainScreen{tabs, current_tab_index: 0, event_sender, pr: None}
     }
 
     pub fn set_pr(&mut self, pr: pr::Pr) {
         self.pr = Some(pr);
+        self.event_sender.send(AppEvent::ScreenRepaint).unwrap();
     }
 }
 
-impl  DrawableScreen  for MainScreen  {
+impl DrawableScreen for MainScreen  {
     fn draw(&self, buffer: &mut dyn Write, rect: Rect) {
         let screen_rect = Rect {y: rect.y + 1, h: rect.h - 1, ..rect};
-        let screen = Screen::new(screen_rect);
-        screen.draw_border(buffer);
+        let tab_screen = Screen::new(screen_rect);
+        tab_screen.draw_border(buffer);
 
         let mut title_offset : usize = 0;
-        for (title, tab) in self.tabs {
-            
+        for (index, (title, tab)) in self.tabs.iter().enumerate() {
+            let title = format!("{}. {}", index + 1, title);
+            write!(buffer, "{bg}{fg}{go}{title}{nobg}{nofg}",
+                   bg = termion::color::Bg(termion::color::LightBlack),
+                   fg = termion::color::Fg(termion::color::White),
+                   go = Goto(rect.x + title_offset as u16 + 1, rect.y + 1),
+                   title = title,
+                   nobg = termion::color::Bg(termion::color::Reset),
+                   nofg = termion::color::Fg(termion::color::Reset)).unwrap();
+            tab.draw(buffer, tab_screen.get_content_rect());
+            title_offset += title.len();
         }
         buffer.flush().unwrap();
     }
 }
 
-impl  InteractableScreen for MainScreen  {
+impl InteractableScreen for MainScreen  {
     fn validate_input(&self, input: u8) -> bool {
         false
     }
